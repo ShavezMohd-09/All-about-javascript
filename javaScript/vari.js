07e5f9ac26239eb8118a54db4b1d10dbb90312c7
Mohd Shavez <shavezmohd7997@gmail.com>
2024-11-11 07:26:15 +0000

    variable in js

---
+++ javaScript/vari.js
@@ -0,0 +1,15 @@
+//constant variable don't declared again or change....
+const var1 = "shavez";
+const var2 = "mohd shavez";
+console.log(var1);
+
+// let variable don't declared again but be can change its...
+let let_var = "aryan";
+let_var = "shavez aryan";
+console.log(let_var);
+
+// var variable do anything like declared again change..
+
+var var_var = "hello shavez";
+var_var = "shavezhello";
+console.log(var_var);
