336c96f3a5a8f014a3edd8b61e635a81f12b131a
Mohd Shavez <shavezmohd7997@gmail.com>
2024-11-11 14:24:15 +0000

    data type in js

--- javaScript/datatype.js
+++ javaScript/datatype.js
@@ -1,21 +1,33 @@
 // Data type in js
 console.log("primitive datatype");
 // number, string, boolean, undefined, null... 
+
+
+//number datatype 
 let num = 123;
 console.log(num);
 console.log(typeof num)
 
+//string datatype 
 let str = "this is string datatype";
 console.log(str);
 console.log(typeof str)
 
+//boolean datatype 
 let bool = true;
 console.log(bool);
 console.log(typeof bool)
 
+//undefined datatype 
 let data_undef;
 console.log(data_undef);
-console.log(typeof data_null)
+console.log(typeof data_undef)
+
+
+//null datatype 
+let null_data = "";
+console.log(null_data)
+console.log(typeof null_data)
 
 console.log("Non primitive data")
 //array , object
@@ -29,3 +41,4 @@ console.log(my_array.length);
 
 //find datatype of array
 console.log(typeof my_array)
+
