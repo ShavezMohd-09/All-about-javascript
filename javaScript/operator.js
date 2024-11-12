742cc9b5838894cae9d623723594c940504dc6a4
Mohd Shavez <shavezmohd7997@gmail.com>
2024-11-12 11:37:55 +0000

    operator in js

---
+++ javaScript/operator.js
@@ -0,0 +1,45 @@
+// operator in js
+
+//(1)  Arthmetic operator
+
+//declared two varible
+let num1 = 6;
+let num2 = 5;
+
+//addtion
+let sum = num1 + num2;
+console.log("the value of sum after num1 + num2 is : ",sum);
+
+
+// substract
+let sub = num1 - num2;
+console.log("the value of sub after num1 + num2 is : ",sub);
+
+//multiply
+let mul = num1 * num2;
+console.log("the value of mul after num1 + num2 is : ",mul);
+
+//division
+let div = num2 / num2;
+console.log("the value of div after num1 + num2 is : ",div);
+
+//modules(remainder)
+let mod = num1 % num2;
+console.log("the value of mod after num1 + num2 is : ",mod);
+
+//exp(power)
+let exp = num1 ** num2;
+console.log("the value of exp after num1 + num2 is : ",exp);
+
+
+//(1) Assignment operator 
+
+
+
+
+
+
+
+
+
+
