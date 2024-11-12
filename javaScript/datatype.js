cafc58505a2482be7cd100574805549fa4cc5e2b
Mohd Shavez <shavezmohd7997@gmail.com>
2024-11-12 08:13:33 +0000

    datatype in js

--- javaScript/datatype.js
+++ javaScript/datatype.js
@@ -1,6 +1,6 @@
 // Data type in js
 console.log("primitive datatype");
-// number, string, boolean, undefined, null... 
+// number, string, boolean, undefined, null, Bigint... 
 
 
 //number datatype 
@@ -29,16 +29,39 @@ let null_data = null;
 console.log(null_data)
 console.log(typeof null_data) //its null but return object..
 
-console.log("Non primitive data")
-//array , object
+//BigInt datatype
+let num_bigint = BigInt(123);
+console.log(num_bigint)
+console.log(typeof num_bigint)
 
-//create a array
+
+console.log("Non primitive data")
+//  it is a colllection of value..
+
+// array , object
+
+// create a array
 let my_array =["shavez","aryan", 123 , true, null];
 console.log(my_array)
 
-//length of a array
+// length of a array
 console.log(my_array.length);
 
-//find datatype of array
+//f ind datatype of array
 console.log(typeof my_array)
+
+
+// object. (key : value)
+const studentData = {
+    fullname : "md shavez",
+    age : 22,
+    course : "MCA",
+    isPass : true
+}
+studentData["fullname"] = "shavez aryan"; // updated
+console.log(studentData);
+console.log(typeof studentData);
+console.log(studentData.fullname); // access key name in object
+console.log(studentData["fullname"]);  // access key name in object
+
 
