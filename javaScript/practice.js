b1519fd1ce7e1f23d2cc4abaea0e3594dad2b98b
Mohd Shavez <shavezmohd7997@gmail.com>
2024-11-12 10:39:27 +0000

    practice question

---
+++ javaScript/practice.js
@@ -0,0 +1,23 @@
+// create a Object
+// shop item object
+const product = {
+    fullName : "Pen",
+    rating : "1-4",
+    isDeal : true,
+    offer : "30 off",
+    price : 100
+
+}
+console.log(product);
+
+
+//instagram profile object
+const profile = {
+    fullName : "@shavez_aryan",
+    post : 20,
+    followers : 200,
+    follings : 300,
+    isfollow : true,
+    bio : " Being alone not a voring..."
+}
+console.log(profile);
