ebf3588fde83f6c8a636f7efe0d9a18c512a6478
Mohd Shavez <shavezmohd7997@gmail.com>
2024-11-13 07:20:21 +0000

    condition statement in js

---
+++ javaScript/state.js
@@ -0,0 +1,33 @@
+// conditional statement
+
+// if statement (when the if statement is true than they execute else not)
+// if condition is true
+let age = 22;
+if(age >= 18){
+    console.log("yor have to right for vote");
+}
+// if condition is false
+let age1 = 16;
+if(age1 >= 18){
+    console.log("yor have to right for vote");
+}
+
+// if else statement
+
+// (the if else statement like true or false )
+// when condition is true
+let marks = 75;
+if(marks >= 45){
+    console.log("you are pass in this session...");
+}else{
+    console.log("yor are failed in this session...");
+}
+
+
+// when condition is false
+let marks2 = 44;
+if(marks2 >= 45){
+    console.log("you are pass in this session...");
+}else{
+    console.log("yor are failed in this session...");
+}
